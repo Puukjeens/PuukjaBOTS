349a79e74dea58282080f573ef45bd9e8acea87f
Puukjeens <klaza@tvnet.lv>
2015-07-04 15:53:19 +0300

    Update PuukjeenaBOTS.js

--- PuukjeenaBOTS.js
+++ PuukjeenaBOTS.js
@@ -35,18 +35,7 @@
 
          */
          
-                bot.commands.PuukjeensCommand = {
-         command: 'Puukjeens',
-         rank: 'user',
-         type: 'exact',
-         functionality: function(chat, cmd){
-         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-         if( !bot.commands.executable(this.rank, chat) ) return void (0);
-         else{
-        API.sendChat("/Mīlumins Puukjeens labakais!!!");
-         }
-         }
-         };
+        
 
 
         bot.commands.baconCommand = {
